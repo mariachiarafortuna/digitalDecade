bf976bb6ac82ac8aa0a556d30b49351368b56637
mariachiarafortuna <mariachiara.fortuna1@gmail.com>
2024-10-25 12:16:24 +0200

    ADD data reading 2023 e 2024

--- traiettoria_servizi_digitali.R
+++ traiettoria_servizi_digitali.R
@@ -4,5 +4,53 @@ require(dplyr)
 # require(tidyr)
 # require(stringr)
 
-mapping <- read_excel(here("inst/data/Mapping cluster e IPA.xlsx"),
-                      sheet = "Mapping cluster vs IPA")
+
+#--- Anno 2023
+nat23_res <- read_excel(here("data/6_eGovernment_Benchmark_2023__Final_Results.xlsx"),
+                      sheet = "3a. Nat. Services - Results", skip = 6, na = ".") %>%
+  filter(Country == "IT")
+
+nat23_data <- read_excel(here("data/6_eGovernment_Benchmark_2023__Final_Results.xlsx"),
+                        sheet = "3b. Nat. Services - Data", skip = 5, na = ".") %>%
+  filter(Country == "IT")
+
+cb23_res <- read_excel(here("data/6_eGovernment_Benchmark_2023__Final_Results.xlsx"),
+                       sheet = "4a. CB Services - Results", skip = 6, na = ".") %>%
+  filter(Country == "IT")
+
+cb23_data <- read_excel(here("data/6_eGovernment_Benchmark_2023__Final_Results.xlsx"),
+                        sheet = "4b. CB Services - Data", skip = 6, na = ".") %>%
+  filter(Country == "IT")
+
+#--- Anno 2024
+# Struttura identica a 2023, cambia solo il file
+
+nat24_res <- read_excel(here("data/6_eGovernment_Benchmark_2024__Final_Results.xlsx"),
+                        sheet = "3a. Nat. Services - Results", skip = 6, na = ".") %>%
+  filter(Country == "IT")
+
+nat24_data <- read_excel(here("data/6_eGovernment_Benchmark_2024__Final_Results.xlsx"),
+                         sheet = "3b. Nat. Services - Data", skip = 5, na = ".") %>%
+  filter(Country == "IT")
+
+cb24_res <- read_excel(here("data/6_eGovernment_Benchmark_2024__Final_Results.xlsx"),
+                        sheet = "4a. CB Services - Results", skip = 6, na = ".") %>%
+  filter(Country == "IT")
+
+cb24_data <- read_excel(here("data/6_eGovernment_Benchmark_2024__Final_Results.xlsx"),
+                         sheet = "4b. CB Services - Data", skip = 6, na = ".") %>%
+  filter(Country == "IT")
+
+overall_eb_scores <- read_excel(here("data/6_eGovernment_Benchmark_2024__Final_Results.xlsx"),
+                                sheet = "1. Scores 2024", skip = 5, na = ".") %>%
+  filter(Country == "IT")
+
+# # Irrilevante per traiettoria nazionale, probabilmente utile per non scored indicators
+# nat24_portal <- read_excel(here("data/6_eGovernment_Benchmark_2024__Final_Results.xlsx"),
+#                          sheet = "5. Nat. Portals - Data", skip = 5) %>%
+#   filter(Country == "IT")
+
+
+###### FOCUS ONLINE AVAILABILITY
+
+
