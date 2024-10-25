647985e974bf124536f33cfd2838b5e2f08736ea
mariachiarafortuna <mariachiara.fortuna1@gmail.com>
2024-10-25 13:33:54 +0200

    ADD Stats: check provider e campionamento

--- traiettoria_servizi_digitali.R
+++ traiettoria_servizi_digitali.R
@@ -59,6 +59,15 @@ oa_24 <- nat24_data[,c(1,3:15)] %>%
          A2_available_service = case_when(`A2: service available online?` == "Yes" ~ 1, TRUE ~ 0),
          A3_available_portal = case_when(`A3: available through portal?` == "Yes" ~ 1, TRUE ~ 0))
 
+oa_23 <- nat23_data[,c(1,3:15)] %>%
+  arrange(`Life event`, `Service`) %>%
+  mutate(A1_available_info = case_when(`A1: information available online?` == "Yes" ~ 1, TRUE ~ 0),
+         A2_available_service = case_when(`A2: service available online?` == "Yes" ~ 1, TRUE ~ 0),
+         A3_available_portal = case_when(`A3: available through portal?` == "Yes" ~ 1, TRUE ~ 0))
+
+
+# Stats servizi e life events
+
 summary_nat_services <- oa_24 %>%
   group_by(`Life event`, `Service`) %>%
   summarise(Portali = n(),
@@ -72,3 +81,26 @@ summary_nat_life_events <- oa_24 %>%
             `Informazione disponibile` = round(sum(A1_available_info)/n()*100,2),
             `Servizio disponibile` = round(sum(A2_available_service)/n()*100,2),
             `Presenza nel portale` = round(sum(A3_available_portal)/n()*100,2))
+
+# Stats tipologia attore
+summary_nat_provider_24 <- oa_24 %>%
+  group_by(`Service Provider`) %>%
+  summarise(`Servizi esaminati` = n(),
+            `Informazione disponibile` = round(sum(A1_available_info)/n()*100,2),
+            `Servizio disponibile` = round(sum(A2_available_service)/n()*100,2),
+            `Presenza nel portale` = round(sum(A3_available_portal)/n()*100,2))
+
+
+# Check campionamento
+campionamento24 <- oa_24 %>% select(provider24 = `Service Provider`, servizio24 = `Service`) %>%
+  arrange(provider24, servizio24)
+campionamento23 <- oa_23 %>% select(provider23 = `Service Provider`, servizio23 = `Service`) %>%
+  arrange(provider23, servizio23)
+
+campionamento <- cbind(campionamento24, campionamento23) %>%
+  mutate(check_provider = (provider24 == provider23),
+         check_servizio = (servizio24 == servizio23))
+sum(campionamento$check_provider)
+sum(campionamento$check_servizio)
+
+# nessun campionamento
